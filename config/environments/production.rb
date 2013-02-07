a373406521599c0ab30de04473ba93b52b6fff0e
Scott Bartell <me@scottbartell.com>
2013-02-06 21:19:05 -0500

    Config cdn url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ ScottBartell::Application.configure do
   config.cache_store = :dalli_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "http://cdn.scottbartell.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
